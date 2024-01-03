af8a03f97022791c2692f768efa8074b45946f07
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-02 17:55:28 -0800

    Store first player's spot selection

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+const player = (function () {
+  const players = {
+    player1: {
+      token: 1,
+    },
+    player2: {
+      token: 2,
+    },
+  };
+
+  const activePlayer = players.player1;
+  
+  const playerSelection = [];
+
+  const select = (row, column) => {
+    playerSelection.push(row, column);
+  };
+
+  const getPlayerSelection = () => playerSelection;
+
+  return {
+    select,
+    getPlayerSelection,
+  }
+})();
+
 const gameboard = (function() {
-  console.log("Player 1 starts (X). Pick a spot by entering `select(<row index>, <column index>)");
+  console.log("Player 1 starts (X). Pick a spot by entering `player.select(<row index>, <column index>)");
 
   const board = [
     [0,0,0],
