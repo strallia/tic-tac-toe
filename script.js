617d25c8c651ff64e1d7de0ec05475a4618d7540
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-02 19:18:59 -0800

    Display updated board after first player's selection

--- script.js
+++ script.js
@@ -1,41 +1,69 @@
+const gameFlow = (function () {
+  console.log("Player 1 starts (X). Pick a spot by entering `player.select(<row index>, <column index>)`");
+
+  const gameMaster = (prevStep) => {
+    if (prevStep === 'player selection') {
+      gameboard.updateBoard()
+      gameboard.displayBoard();
+      console.log("Player 2's turn (O)");
+    }
+  };
+
+  return {
+    gameMaster,
+  };
+})();
+
 const player = (function () {
   const players = {
     player1: {
-      token: 1,
+      token: 'X',
     },
     player2: {
-      token: 2,
+      token: 'O',
     },
   };
 
   const activePlayer = players.player1;
   
-  const playerSelection = [];
+  let playerSelection = [];
 
   const select = (row, column) => {
+    playerSelection = [];
     playerSelection.push(row, column);
+    gameFlow.gameMaster('player selection');
   };
 
   const getPlayerSelection = () => playerSelection;
 
+  const getPlayerToken = () => activePlayer.token;
+
   return {
     select,
     getPlayerSelection,
+    getPlayerToken,
   }
 })();
 
-const gameboard = (function() {
-  console.log("Player 1 starts (X). Pick a spot by entering `player.select(<row index>, <column index>)");
 
+const gameboard = (function () {
   const board = [
     [0,0,0],
     [0,0,0],
     [0,0,0]
   ];
 
+  const updateBoard = () => {
+    const chosenPosition = player.getPlayerSelection();
+    board[chosenPosition[0]][chosenPosition[1]] = player.getPlayerToken();
+  };
+
+  const displayBoard = () => console.log(board);
+
   return {
     board,
+    updateBoard,
+    displayBoard,
   };
 })();
-
 console.log(gameboard.board);
