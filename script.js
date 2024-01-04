ef29cbc0df71d12d5d9a5cf9d14192f6db2780aa
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-03 16:15:00 -0800

    Check for winner after each round

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const gameFlow = (function () {
 
   const gameMaster = (prevStep) => {
     if (prevStep === 'player selection') {
-      gameboard.updateBoard()
+      gameboard.updateBoard();
       gameboard.displayBoard();
+      if (gameboard.checkWinner()) {
+        console.log('MASTER: Winner found! Game complete.');
+        return;
+      };
       player.setNextPlayer();
       player.announcePlayerTurn();
     }
@@ -29,19 +33,17 @@ const player = (function () {
   };
 
   let activePlayer = players.player1;
-  
   let playerSelection = [];
+
+  const getPlayerSelection = () => playerSelection;
+  const getPlayerToken = () => activePlayer.token;
+  const getPlayerName = () => activePlayer.name;
 
   const select = (row, column) => {
     playerSelection = [];
     playerSelection.push(row, column);
     gameFlow.gameMaster('player selection');
   };
-
-  const getPlayerSelection = () => playerSelection;
-  const getPlayerToken = () => activePlayer.token;
-  const getActivePlayer = () => activePlayer;
-
   const setNextPlayer = () => {
     if (activePlayer === players.player1) {
       activePlayer = players.player2;
@@ -49,7 +51,6 @@ const player = (function () {
       activePlayer = players.player1;
     }
   };
-
   const announcePlayerTurn = () => {
     console.log(`${activePlayer.name}'s turn (${activePlayer.token})`);
   }
@@ -58,6 +59,7 @@ const player = (function () {
     select,
     getPlayerSelection,
     getPlayerToken,
+    getPlayerName,
     setNextPlayer,
     announcePlayerTurn,
   }
@@ -75,13 +77,70 @@ const gameboard = (function () {
     const chosenPosition = player.getPlayerSelection();
     board[chosenPosition[0]][chosenPosition[1]] = player.getPlayerToken();
   };
-
   const displayBoard = () => console.log(board);
+  const checkWinner = () => {
+    const playerToken = player.getPlayerToken();
+    let winner = false;
+
+    const checkFiltered = (arr) => {
+      let filtered = arr.filter((value) => {
+        return value === playerToken;
+      });
+      if (filtered.length === 3) {
+        winner = true;
+      };
+    };
+
+    // check each column
+    for (let colIndex = 0; colIndex < 3; colIndex++) {
+      let column = [];
+      for ( let rowIndex = 0; rowIndex < 3; rowIndex++) {
+        column.push(board[rowIndex][colIndex]);
+      };
+      checkFiltered(column);
+    };
+
+    // check each row
+    for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+      let row = [];
+      for ( let colIndex = 0; colIndex < 3; colIndex++) {
+        row.push(board[rowIndex][colIndex]);
+      };
+      checkFiltered(row);
+    };
+
+    // check backslash diagonal
+    {
+      let diagonal = [];
+      for (let i = 0; i < 3; i++) {
+        diagonal.push(board[i][i]);
+      };
+      checkFiltered(diagonal);
+    }
+
+    // check forwardslash diagonal
+    {
+      let diagonal = [];
+      for (let i = 0; i < 3; i++) {
+        let rowIndex = i;
+        let colIndex = 2 - i;
+        diagonal.push(board[rowIndex][colIndex]);
+      };
+      checkFiltered(diagonal);
+    };
+
+    if (winner) {
+      console.log(`${player.getPlayerName()} wins!`);
+      return true;
+    };
+    console.log(`no winner this round`);
+  };
 
   return {
     board,
     updateBoard,
     displayBoard,
+    checkWinner,
   };
 })();
 console.log(gameboard.board);
